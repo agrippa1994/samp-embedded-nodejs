f1cc9f1e4426bb4bde46ad66c4d64bd9fca3d97b
agrippa1994 <agrippa1994@icloud.com>
2017-06-27 22:52:40 +0200

    remove type definition and add new natives

--- playground/samp.js
+++ playground/samp.js
@@ -1,70 +1,28 @@
 const process = require("process");
 const samp = process.binding("samp");
-
-
-class int {
-    constructor(value) {
-        this.type = "int";
-        this.buffer = new Buffer(4);
-        this.buffer.writeInt32LE(value);
-    }
-};
-
-class float {
-    constructor(value) {
-        this.type = "float";
-        this.buffer = new Buffer(4);
-        this.buffer.writeFloatLE(value);
-    }
-};
-
-class string {
-    constructor(value) {
-        this.type = "string";
-        this.buffer = new Buffer(value + "\0");
-    }
-};
-
-class floatRef {
-    constructor(value) {
-        this.type = "floatRef";
-        this.buffer = new Buffer(4);
-        this.buffer.writeFloatLE(value);
-    }
-
-    get() {
-        return this.buffer.readFloatLE(0);
-    }
-}
-const typeToFormat = {
-    "s": string,
-    "i": int,
-    "d": int,
-    "f": float,
-    "R": floatRef
-};
+const types = require("./samp-types.js");
 
 const nativeToFormat = {
-    "SendClientMessage": "dds"
-    //"GetPlayerPos": "dRRR"
+    "SendClientMessage": "dds",
+    "GetPlayerPos": "dRRR",
+    "SetPlayerPos": "dfff",
+    "GetPlayerName": "dRd"
 };
 
 function callNative(native) {
-    console.trace();
-    console.log(JSON.stringify(arguments, null, 4));
     if(!(native in nativeToFormat))
         throw `Invalid native ${native}`;
 
-    nativeFormat = nativeToFormat[native];
+    var nativeFormat = nativeToFormat[native];
     if(arguments.length - 1 != nativeFormat.length)
         throw "Supplied invalid number of arguments.";
 
-    nativeArguments = [];
+    var nativeArguments = [];
     if(arguments.length > 0) {
         for(var argumentIdx = 1; argumentIdx < arguments.length; ++argumentIdx) {
             const argument = arguments[argumentIdx];
             const typeIdentifier = nativeFormat.charAt(argumentIdx - 1);
-            const type = typeToFormat[typeIdentifier];
+            const type = types.typeForFormat(typeIdentifier);
 
             if(!(argument instanceof type))
                 throw `Argument ${argument.type} at index ${argumentIdx} does not conform to type ${typeIdentifier}`;
@@ -73,34 +31,22 @@ function callNative(native) {
         }
     }
     console.log("NATIVE " + native + " NativeFormat: " + nativeFormat + ", arguments: " + JSON.stringify(nativeArguments));
-    return samp.invokeNative.apply(null, [native, nativeFormat].concat(nativeArguments));
+    var res = samp.invokeNative.apply(null, [native, nativeFormat].concat(nativeArguments));
+    console.log("NATIVE " + native + " NativeFormat: " + nativeFormat + ", arguments: " + JSON.stringify(nativeArguments));
+    return res;
 }
 
 var natives = {};
 for(var nativeName in nativeToFormat) {
-
-    function X(name, args) {
+    // some tricky workaround in order to create a function for each native
+    // assigning a function directly to the map leads to strange behaviour
+    function invoker(name, args) {
         return callNative.apply(null, [name].concat(args));
     }
-
-    natives[nativeName.charAt(0).toLocaleLowerCase() + nativeName.slice(1)] = X.bind(null, nativeName);
-
+    natives[nativeName.charAt(0).toLocaleLowerCase() + nativeName.slice(1)] = invoker.bind(null, nativeName);
 }
 
-natives.sendClientMessage([new int(5), new int(10), new string("adsfaf")]);
-//natives.getPlayerPos(new int(5), new int(10), new string("adsfaf"));
-
 module.exports = {
-    internal: samp,
-    invokeNative: samp.invokeNative,
     natives, natives,
-    type: {
-        int: int,
-        float: float,
-        string: string
-    },
-
-    invokePublic: () => {
-        return samp.invokeNative.apply(["CallRemoteFunction"] + arguments);
-    }
+    type: types
 }

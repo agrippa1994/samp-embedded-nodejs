146264a7699e552953144ba437d6104a70b08b2e
agrippa1994 <agrippa1994@icloud.com>
2017-06-27 22:51:03 +0200

    move types into seperate file

---
+++ playground/samp-types.js
@@ -0,0 +1,68 @@
+
+module.exports.int = class {
+    constructor(value) {
+        this.type = "int";
+        this.format = "i";
+        this.buffer = new Buffer(4);
+        this.buffer.writeInt32LE(value);
+    }
+
+    static get format() {
+        return "d";
+    }
+};
+
+module.exports.float = class {
+    constructor(value) {
+        this.type = "f";
+        this.buffer = new Buffer(4);
+        this.buffer.writeFloatLE(value);
+    }
+
+    static get format() {
+        return "f";
+    }
+};
+
+module.exports.string = class {
+    constructor(value) {
+        this.type = "string";
+        this.format = "s";
+        this.buffer = new Buffer(value + "\0");
+    }
+
+    static get format() {
+        return "s";
+    }
+};
+
+module.exports.ref = class {
+    constructor(size=4) {
+        this.type = "ref";
+        this.buffer = new Buffer(size);
+    }
+
+    toFloat() {
+        return this.buffer.readFloatLE();
+    }
+
+    toInt() {
+        return this.buffer.readInt32LE();
+    }
+
+    toString(start, end) {
+        return this.buffer.toString("ascii", start, end);
+    }
+
+    static get format() {
+        return "R";
+    }
+}
+
+module.exports.typeForFormat = (format) => {
+    for(type in module.exports) {
+        const constructor = module.exports[type];
+        if(constructor.format === format)
+            return constructor;
+    }
+}

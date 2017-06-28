535d52c6a9a751b2b56561a4fbb394fa49206d91
agrippa1994 <agrippa1994@icloud.com>
2017-06-28 23:48:09 +0200

    invokeNative takes argument count explicitly

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,39 @@
-#include "dispatcher.h"
-#include "node_helpers.h"
-
 #include <sampgdk.h>
-
 #include <node.h>
 #include <node_api.h>
 #include <thread>
-#include <iostream>
-#include <queue>
 
+#include "dispatcher.h"
+#include "node_helpers.h"
 
-#include "string.h"
 
 #define UNUSED(x) (void)x;
-
 
 Dispatcher dispatcher;
 
 namespace nsamp
 {
-
     napi_value invokeNative(napi_env env, napi_callback_info info) {
         NodeScope scope(env);
         UNUSED(scope)
 
-        napi_value argv[10], thisPtr;
+        napi_value argv[32], thisPtr;
         size_t argc = sizeof(argv) / sizeof(argv[0]);
         napi_get_cb_info(env, info, &argc, argv, &thisPtr, nullptr);
 
-        if(argc < 1) {
+        if(argc < 3) {
             napi_throw_error(env, "Invalid number of arguments");
             return 0;
         }
 
-        napi_valuetype valueType;
-
         // fetch native string
-        napi_typeof(env, argv[0], &valueType);
-        if(valueType != napi_string) {
+        char nativeString[128];
+        if(napi_get_value_string_utf8(env, argv[0], nativeString, sizeof(nativeString), nullptr) != napi_ok) {
             napi_throw_error(env, "First argument must be a string");
             return 0;
         }
 
-        char nativeString[128];
-        napi_get_value_string_utf8(env, argv[0], nativeString, sizeof(nativeString), nullptr);
+        // verify native name
         AMX_NATIVE native = sampgdk_FindNative(nativeString);
         if(native == nullptr) {
             napi_throw_error(env, "Native can not be found");
@@ -52,26 +42,28 @@ namespace nsamp
 
         // fetch format
         char formatString[128];
-        if(argc >= 2) {
-            napi_typeof(env, argv[1], &valueType);
-            if(valueType != napi_string) {
-                napi_throw_error(env, "Second argument must be a string");
-                return 0;
-            }
-            napi_get_value_string_utf8(env, argv[1], formatString, sizeof(formatString), nullptr);
+        if(napi_get_value_string_utf8(env, argv[1], formatString, sizeof(formatString), nullptr) != napi_ok) {
+            napi_throw_error(env, "Second argument must be a string");
+            return 0;
         }
 
-        // check if format string has elements and nodejs has arguments on the stack
-        if(strlen(formatString) > 0 && (argc - 2 <= 0)) {
-            napi_throw_error(env, "Format string has elements but no arguments");
+        // fetch number of arguments
+        uint32_t numberOfArgs = 0;
+        if(napi_get_value_uint32(env, argv[2], &numberOfArgs) != napi_ok) {
+            napi_throw_error(env, "Third argument must be a number");
+            return 0;
+        }
+
+        if(argc - 3 != numberOfArgs) {
+            napi_throw_error(env, "Number of native arguments must match the number of arguments");
             return 0;
         }
 
         // create arguments for pawn
         void **pawnArgArray = (void **)1; // hack, sampgdk does not allow nullptr as pawn arguments array
-        if(argc > 2) {
-            pawnArgArray = new void *[argc - 2];
-            for(int nodeIdx = 2, pawnIdx = 0; nodeIdx < argc; ++nodeIdx, ++pawnIdx) {
+        if(numberOfArgs > 0) {
+            pawnArgArray = new void *[numberOfArgs];
+            for(int nodeIdx = 3, pawnIdx = 0; nodeIdx < argc; ++nodeIdx, ++pawnIdx) {
                 napi_get_buffer_info(env, argv[nodeIdx], &pawnArgArray[pawnIdx], nullptr);
             }
         }
@@ -82,10 +74,10 @@ namespace nsamp
         });
 
         // clean up
-        if(argc > 2) {
+        if(numberOfArgs > 0) {
             delete pawnArgArray;
         }
-    
+
         napi_value returnValue;
         napi_create_number(env, static_cast<double>(gdkReturnValue), &returnValue);
         return returnValue;
